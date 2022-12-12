643cea94e23a559edea077a7f7cacec9237017f0
ZilvisP <zilvinas.paezerinis@gmail.com>
2022-12-12 15:21:08 +0200

    Mano pirmasis komitas

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const x = 5;
+const y = 6;
+const z = x + y;
+console.log(z);
+//pirmoji uzduotis
+let a = 1;
+const b = " Kintamoji a reiksme";
+console.log(b + ' ' + a);
+a = 2;
+console.log (`Kintamoji a reiksme ${a}`)
+
+//antroji uzduotis
+
+const manoVardas = "Zilvis Pilvis";
+const tekstas = 'Mano vardas'
+console.log(`${tekstas} ${manoVardas}`);
+console.log(tekstas + ` ` + manoVardas)
+//trecioji uzduotis
+
+const metai = 1989;
+const menuo = 6;
+const diena = 19;
+let mew = metai%10 + menuo%7 + diena%10;
+console.log(mew);
